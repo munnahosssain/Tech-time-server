093ee709932a4f8c3fcae7e3e12e7ea04892094a
Munna <mayinuddin.munna@gmaile.com>
2022-10-12 14:50:07 +0600

    connect with DB

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
+const cors = require('cors');
 
 app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kojbjjk.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const collection = client.db("techTime").collection("test");
+
+        app.get('/test', async (req, res) => {
+            const query = {};
+            const cursor = collection.find(query);
+            const services = await cursor.toArray(cursor);
+            res.send(services);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send("Hello world!");
